1e951f0dd42bf4f4213b508138677727fcd15dd9
maidOutfitMarsupial <maidOutfitMarsupial@proton.me>
2023-03-21 15:25:27 -0700

    switched the loop into python threading. Need to plan improvements

--- temp.py
+++ temp.py
@@ -1,5 +1,32 @@
-import subprocess
+import subprocess, time
+import time
+import threading
 
-p = subprocess.Popen(["ls", "-la"])
-p.wait()
-print("result:", p)
+# use threading.Timer for SpotifyReloader
+#  and stop writing in the sleep yourself.
+# otherwise, InputThread should gain the functionality of skipping
+#  (and subsequently cancelling and recreating a SpotifyReloader thread)
+#  pausing, playing, and printing song details.
+
+class SpotifyReloader(threading.Thread):
+    def run(self):
+        while True:
+            result = subprocess.check_output(["osascript", "./script.osascript"])
+            result = str(result.decode("utf-8")).strip()
+            time.sleep(float(result))
+
+class InputThread(threading.Thread):
+    def run(self):
+        quit = False
+        while quit != True:
+            response = input("instruction: ")
+            print(f"you said {response}")
+            if response == "quit":
+                quit = True
+
+if __name__ == "__main__":
+    spot_t = SpotifyReloader()
+    spot_t.start()
+    in_t = InputThread()
+    in_t.start()
+    
