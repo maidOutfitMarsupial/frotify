65c1afe706941613d4c59ad7ce2bb951ecb2a059
maidOutfitMarsupial <maidOutfitMarsupial@proton.me>
2023-03-27 20:41:37 -0700

    holy shit, I know nothing about threading. Save me.

--- skipper.py
+++ skipper.py
@@ -14,20 +14,36 @@ class SpotifyReloader(threading.Thread):
         while True:
             result = subprocess.check_output(["osascript", "./reloadSpotify.osascript"])
             result = str(result.decode("utf-8")).strip()
+            print(result)
             time.sleep(float(result))
 
 class InputThread(threading.Thread):
+    def __init__(self):
+        behavioral_dict = dict()
+        behavioral_dict.update(dict.fromkeys(["quit"], (self.set_quit_true,)))
+        behavioral_dict.update(dict.fromkeys(["reload"], (SpotifyReloader().start,)))
+        self.switch = behavioral_dict
+        self.quit = False
+        threading.Thread.__init__(self)
+
     def run(self):
-        quit = False
-        while quit != True:
+        while self.quit != True:
             response = input("instruction: ")
-            print(f"you said {response}")
-            if response == "quit":
-                quit = True
+            requested_behavior = self.switch[response] if response in self.switch else None
+            if requested_behavior == None:
+                print(f"{response} isn't a valid response")
+            else:
+                requested_fn = requested_behavior[0]
+                requested_params = requested_behavior[1:]
+                requested_fn(*requested_params)
+
+    def set_quit_true(self):
+        self.quit = True
 
 if __name__ == "__main__":
+
     spot_t = SpotifyReloader()
+    spot_t.setDaemon(True)
     spot_t.start()
     in_t = InputThread()
     in_t.start()
-    
